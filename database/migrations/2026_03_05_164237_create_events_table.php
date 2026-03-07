2c901eecea942b3f374e84071425df562605d3de
Noreen Chepkurui <noreenkoskeii7@gmail.com>
2026-03-07 14:29:46 +0300

    add Events API with fellowship info and route

--- database/migrations/2026_03_05_164237_create_events_table.php
+++ database/migrations/2026_03_05_164237_create_events_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->unique();
+            $table->foreignId('fellowship_id')->constrained()->onDelete('cascade');
+            $table->dateTime('date_of_event');
             $table->timestamps();
         });
     }
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\FellowshipController;
+use App\Http\Controllers\EventController;
 
 Route::get('/check-user/{phone}',  [UserController::class, 'checkUser']);
 
@@ -11,3 +12,5 @@ Route::post('/user', [UserController::class, 'store']);
 Route::get('/users', [UserController::class, 'index']);
 
 Route::get('/fellowships', [FellowshipController::class, 'index']);
+
+Route::get('/events', [EventController::class, 'index']);
